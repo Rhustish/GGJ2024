494988a1456a8770f53ab8b7b2eaa7c8516ef8d1
Rhustish <bbhatnagat27@gmail.com>
2024-01-28 03:55:45 +0530

    main menu fix

--- Sock-it/Assets/Scripts/MainMenu.cs
+++ Sock-it/Assets/Scripts/MainMenu.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public void onPlay()
     {
-        // Debug.Log("ho gya");
-        SceneManager.LoadScene("Level");
+        Button obj = gameObject.GetComponent<Button> ();
+        obj.interactable = false;
+        TextMeshProUGUI child = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        child.SetText("Already Began");
+    }
+
+    public void onContinue(){
+        SceneManager.LoadScene("Level-Main",LoadSceneMode.Single);
     }
 
     public void onQuit()
